b898a41188e19282ab873193ab2561a50b7c918f
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-08-27 16:45:44 +0300

    refactor: update attribution text and author links in README and component

--- src/components/layout/attribution.tsx
+++ src/components/layout/attribution.tsx
@@ -1,16 +1,7 @@
 export default function Attribution() {
   return (
     <p className="pb-0.5 text-center text-body-sm text-grey-400">
-      Challenge by{" "}
-      <a
-        className="v-credit-link"
-        href="https://www.frontendmentor.io?ref=challenge"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Frontend&nbsp;Mentor
-      </a>
-      . Coded by{" "}
+      Coded by{" "}
       <a
         className="v-credit-link"
         href="https://www.linkedin.com/in/abdelrhman-vanta/"
